2b2f3e8d693649f03e5b69e2d0b3e242ba13a145
Federico Barabas <barabas@df.uba.ar>
2013-11-22 23:11:51 +0100

    Changing

--- ontiming.py
+++ ontiming.py
@@ -14,8 +14,9 @@ from pint import UnitRegistry
 ureg = UnitRegistry()
 Q_ = ureg.Quantity
 
-def dgauss(x, y, center, fwhm):
-    return np.exp(-(x - center)**2 / (2 * fwhm**2)) * np.exp(-(y - center)**2 / (2 * fwhm**2))
+def dgauss(x, y, A, center, fwhm):
+    return np.ceil((A / 15) * np.exp(-(x - center)**2 / (2 * fwhm**2)) * 
+                   np.exp(-(y - center)**2 / (2 * fwhm**2)))
 
 # Constant definitions
 nm_per_pixel = 133.0
@@ -36,29 +37,35 @@ def ontiming():
     xx, yy = np.meshgrid(x, y, sparse=True)
     
     # PSF calculation
-    psf = 3 * dgauss(xx, yy,
-                       np.floor(trc_window_l.magnitude / 2),
-                       psf_width.to(ureg.px).magnitude)
+    psf = dgauss(xx, yy, 
+                 exposure * emission_rate, 
+                 np.floor(trc_window_l.magnitude / 2),
+                 psf_width.to(ureg.px).magnitude)
     
     # Time goes from 0 ms to 3 * ontime with a step two orders of
     # magnitude better than the exposure time
-    resolution = exposure / 100
     t_window = 3 * ontime
-    time = np.arange(0, t_window.magnitude, resolution.magnitude)
+    time = np.arange(0, t_window.magnitude)
     
     t_ons = np.arange(4, 5, 0.1) * ureg.millisecond
     
-    n = 100
-    ratio = np.zeros((t_ons.size, n))
+    trials = 1000
+    ratio = np.zeros((t_ons.size, trials))
     
     # Windows with gaussian noise incorporated
-    windows = np.random.normal(loc=0,
-                               scale=5,
-                               size=(t_ons.size,
-                                     n,
-                                     time.size,
-                                     trc_window_l.magnitude,
-                                     trc_window_l.magnitude))
+#    windows = np.random.normal(loc=0,
+#                               scale=0,
+#                               size=(t_ons.size,
+#                                     n,
+#                                     time.size,
+#                                     trc_window_l.magnitude,
+#                                     trc_window_l.magnitude))
+    # If you want it without gaussian noise
+    windows = np.zeros((t_ons.size, 
+                        n, 
+                        time.size, 
+                        trc_window_l.magnitude, 
+                        trc_window_l.magnitude))
     
     # Photon's contribution to signal
     win_photons = np.random.poisson(lam=psf,
@@ -91,14 +98,15 @@ def ontiming():
     trace = signal.sum(axis=(3,4))
     ratio = trace[:, :, 4] / trace[:, :, 5]
     
-    plt.plot(t_ons, ratio.mean(1), 'bo')
+#    plt.plot(t_ons, ratio.mean(1), 'bo')
+    plt.plot(t_ons, ratio.std(1))
     plt.grid('on')
     plt.show()
     
-#    for i in range(len(ratio)):
-    #    print(ratio[i].mean())
-    #    print(ratio[i].std())
-#        plt.plot(trace[i, 0])
+    for i in range(len(ratio)):
+#        print(ratio[i].mean())
+#        print(ratio[i].std())
+        plt.plot(trace[i, 0])
     
     #plt.grid('on')
     #plt.xlabel('Time [{}]'.format(ontime.units))
